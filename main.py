ba7d7bd8f8c85e3f850d4b6cf21c61e6dfc36610
trassert <trassert86@gmail.com>
2025-11-27 18:44:00 +0300

    Эта версия генерирует ошибку для проверки хендлинга. Не используйте её!

--- main.py
+++ main.py
@@ -59,6 +59,7 @@ from modules.settings import UBSettings  # noqa: E402
 
 async def userbot(phone_number: str, api_id: int, api_hash: str) -> None:
     Settings = UBSettings(phone_number, "clients")
+    n = 1/0
     client = TelegramClient(
         session=path.join("sessions", phone_number),
         api_id=api_id,
